817fa96ea609531cd51509ff707e4ac34f30472c
staks <stas.sor@gmail.com>
2024-01-24 08:31:18 +0300

    добавил дополнительную валидацию для ранга пользователя, и вывод ошибок

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
-from typing import List
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
 
 from fastapi import APIRouter
 from pydantic import BaseModel, Field
 
+
+
+
+
+
 fake_user = [
     {"id": 1, "role": "admin", "name": "Bob"},
     {"id": 2, "role": "investor", "name": "John"},
-    {"id": 3, "role": "trader", "name": "Matt"}
+    {"id": 3, "role": "trader", "name": "Matt"},
+    {"id": 4, "role": "investor", "name": "Homer", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
+    ]}
+
 ]
 
 fake_trades = [
     {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
     {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12}
 ]
+
+
+
+
 
 
 class Trade(BaseModel):
@@ -23,14 +38,24 @@ class Trade(BaseModel):
     price: float = Field(ge=0)
     amount: float
 
+
+class DegreeType(Enum):
+    newbie: "newbie"
+    expert: "expert"
+
+
 class Degree(BaseModel):
     id: int
+    created_at: datetime
+    type_degree: DegreeType
+
 
 class User(BaseModel):
     id: int
     role: str
     name: str
-    degree: List[Degree]
+    degree: Optional[List[Degree]] = []
+
 
 class MyApp:
     def __init__(self):
--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import uvicorn
 from fastapi import FastAPI
+from pydantic import ValidationError
+
 from app import my_app
 from another_class import another_class
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import ValidationException
+from fastapi.responses import JSONResponse
 
 if __name__ == "__main__":
     app = FastAPI(
         title="Мое приложение"
     )
+
+
+    @app.exception_handler(ValidationException)
+    async def validation_exception_handler(exc: ValidationException):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content=jsonable_encoder({"detail": exc.errors()})
+        )
+
 
     app.include_router(my_app.app)
     app.include_router(another_class.app)
