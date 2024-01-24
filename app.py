426bf2e271173ea418c17215bd5655248288b4a0
staks <stas.sor@gmail.com>
2024-01-24 09:17:47 +0300

    передвинул

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ from typing import List, Optional
 from fastapi import APIRouter
 from pydantic import BaseModel, Field
 
-
-
-
-
-
 fake_user = [
     {"id": 1, "role": "admin", "name": "Bob"},
     {"id": 2, "role": "investor", "name": "John"},
@@ -24,10 +19,6 @@ fake_trades = [
     {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
     {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12}
 ]
-
-
-
-
 
 
 class Trade(BaseModel):
