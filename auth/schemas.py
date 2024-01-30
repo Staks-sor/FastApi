9391f4f694d5351c750c659da59e8c66212d82be
staks <stas.sor@gmail.com>
2024-01-30 20:45:02 +0300

    добавил менеджер, схемы и роуты в меин

---
+++ auth/schemas.py
@@ -0,0 +1,35 @@
+import uuid
+from pydantic import ConfigDict
+
+from typing import Optional
+
+from fastapi_users import schemas
+from fastapi_users.schemas import PYDANTIC_V2
+
+
+class UserRead(schemas.BaseUser[int]):
+    id: int
+    email: str
+    username: str
+    role_id: int
+    is_active: bool = True
+    is_superuser: bool = False
+    is_verified: bool = False
+
+    if PYDANTIC_V2:  # pragma: no cover
+        model_config = ConfigDict(from_attributes=True)  # type: ignore
+    else:  # pragma: no cover
+
+        class Config:
+            orm_mode = True
+
+
+class UserCreate(schemas.BaseUserCreate):
+    username: str
+    email: str
+    password: str
+    role_id: int
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi_users import fastapi_users, FastAPIUsers
 from pydantic import ValidationError
 
 from app import my_app
@@ -9,9 +10,31 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import ValidationException
 from fastapi.responses import JSONResponse
 
+from auth.auth import auth_backend
+from auth.database import User
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
+
 if __name__ == "__main__":
     app = FastAPI(
         title="Мое приложение"
+    )
+
+    fastapi_users = FastAPIUsers[User, int](
+        get_user_manager,
+        [auth_backend],
+    )
+
+    app.include_router(
+        fastapi_users.get_auth_router(auth_backend),
+        prefix="/auth/jwt",
+        tags=["auth"],
+    )
+
+    app.include_router(
+        fastapi_users.get_register_router(UserRead, UserCreate),
+        prefix="/auth",
+        tags=["auth"],
     )
 
 
